ea1bb9d00117d7d98836773eb6942efa965678ce
Mike <saburov1985@gmail.com>
2024-09-12 15:32:14 +1100

    реализовать создание, отображение, удаление статьи и показ всех статейй на главной.
    Реализована логика, верстка не сделана

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "articles#index"
-  get "/articles", to: "articles#index"
+  resources :articles
 
 end
